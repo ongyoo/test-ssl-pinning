2446fcf02c2e696fdb47d9948f72c14d6686d80f
Sutham <dev@Bad-Badtz-Maru.local>
2017-08-16 14:09:28 +0700

    first commit

---
+++ test-ssl-pinning/BaseModel.swift
@@ -0,0 +1,13 @@
+//
+//  BaseModel.swift
+//  test-ssl-pinning
+//
+//  Created by Sutham on 8/16/2560 BE.
+//  Copyright © 2560 Komsit. All rights reserved.
+//
+
+import UIKit
+
+class BaseModel: NSObject {
+
+}
---
+++ test-ssl-pinning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test-ssl-pinning
+//
+//  Created by Sutham on 8/16/2560 BE.
+//  Copyright © 2560 Komsit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
